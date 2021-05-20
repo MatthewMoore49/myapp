f8faa5feb1a3cd8c034035aeb928ececdf29189d
Matt <msm3390@gmail.com>
2021-05-20 09:41:40 -0400

    run-deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AddPerson from "./AddPeople";
+import People from './People'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+    state = {
+
+        people : [
+            {name: "Austin", age:'22', favColor:'blue', id:1},
+            {name: "Lashonda", age:'18', favColor:'red', id:2},
+            {name: "Jamie", age:'83', favColor:'yellow', id:3}
+
+        ]
+    }
+    addPerson = (person) => {
+        person.id = Math.random();
+        let people = [...this.state.people, person]
+        this.setState({
+            people: people
+        })
+    }
+    deletePerson = (id) => { 
+       // console.log(id);
+       let people = this.state.people.filter(person => { 
+           return person.id !== id
+       });
+    
+    this.setState({
+        people: people
+    });
+}
+componentDidMount(){
+    console.log('component mounted' );
+}
+componentDidUpdate(prevProps, prevState){
+    console.log('component updated');
+    console.log(prevProps, prevState);
 }
 
-export default App;
+  render(){
+      return (
+          <div className="App">
+              <h1>My first react app </h1>
+              <p>Hey there Big boy!</p>
+              <People people={this.state.people} deletePerson={this.deletePerson}/> 
+              <AddPerson addPerson={this.addPerson} />
+            
+          </div>
+      );
+  }
+  }
+  
+  export default App;
+  
+  
+  
---
+++ src/People.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './People.css';
+    const People = ({people, deletePerson}) => {
+      
+
+     /*const peopleList = people.map(person => {  
+           if(person.age>20){
+        return(
+            <div className="person" key = {person.id}>
+                <div>Name: { person.name }</div>
+                <div>Age: { person.age }</div>
+                <div>favColor: { person.favColor }</div>
+            </div>
+            
+        )
+         }else{
+             return null
+         }
+       } );*/ 
+
+       const peopleList = people.map(person => {
+           return person.age > 21 ? (
+
+             <div className="person" key = {person.id}>
+
+                <div>Name: { person.name }</div>
+                <div>Age: { person.age }</div>
+                <div>favColor: { person.favColor }</div>
+                <button onClick={ () => {deletePerson(person.id)}}>Delete person</button>
+
+            </div>
+                    
+                   
+
+           ) : null 
+       })
+
+
+       return (
+           <div className="people-list">
+            { peopleList }
+           </div>         
+       );
+     
+}
+export default People;
